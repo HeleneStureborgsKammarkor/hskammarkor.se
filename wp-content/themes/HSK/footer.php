7b2e9857c411bc6942dcf3dee599eddd94460b5f
Tomas Lycken <tlycken@kth.se>
2013-08-13 01:43:09 +0200

    Inloggningsmeny med länkar till internsidorna och admin, baserat på privilegier

--- wp-content/themes/HSK/footer.php
+++ wp-content/themes/HSK/footer.php
@@ -10,7 +10,25 @@
     <div class="wrapper_footer">
         <div id='footer'>  
             <div id='footermenu'>
-                <?php wp_nav_menu(array('menu_class' => 'nav_footer', 'theme_location' => 'footer_menu', 'depth' => '1', 'fallback_cb' => false)); ?>
+				<ul id="menu-login-meny" class="nav_footer">
+					<?php if (is_user_logged_in()): ?>
+					<li id="menu-item-1" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-96">
+						<a href="http://hskammarkor.se/internsidorna/">Internsidorna</a>
+					</li>
+					<?php if (current_user_can('manage_options')): ?>
+					<li id="menu-item-2" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-62">
+						<a href="http://hskammarkor.se/wp-admin">Admin</a>
+					</li>
+					<?php endif; ?>
+					<li id="menu-item-3" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-229">
+						<a href="http://hskammarkor.se/wp-login.php?action=logout">Logga ut</a>
+					</li>
+					<?php else: ?>
+					<li id="menu-item-229" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-229">
+						<a href="http://hskammarkor.se/wp-login.php?redirect_to=http%3A%2F%2Fhskammarkor.se%2Finternsidorna">Logga in</a>
+					</li>
+					<?php endif; ?>
+				</ul>
             </div>
             <p align="right">
             &copy; <?php bloginfo('name'); ?> <?php echo date("Y"); ?>. Wordpress-tema av Tomas Lycken.
